90bd4d7d14ce4e1c14baf5cf188dff04ca1b334c
Ma7moudYasser <57537704+Ma7moudYasser@users.noreply.github.com>
2021-06-06 16:36:15 +0200

    Add files via upload

---
+++ new 1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+void insertionSort(int arr[], int n) {
+	int i, j, key;
+	for ( j = 1; j < n; j++)
+	{
+		key = arr[j];
+		i = j - 1;
+		while (i>=0 && arr[i]>key)
+		{
+			arr[i + 1] = arr[i];
+			i--;
+		}
+		arr[i + 1] = key;
+	}
+}
+
+void printarray(int arr[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+int main() {
+	int arr[] = {9,2,7,5,1,4,3,6};
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	insertionSort(arr, n);
+	printarray(arr, n);
+
+
+
+
+
+	system("pause");
+	return 0;
+}
